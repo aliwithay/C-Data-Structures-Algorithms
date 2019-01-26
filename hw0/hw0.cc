6150be7546fe493bca61fb7952a3f8be627277f6
aliwithay <alyammar1@hotmail.com>
2019-01-26 12:53:28 -0700

    hw1

---
+++ hw0/hw0.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "2 - because the first one passed out drunk!\n";
+  return 0;
+}
---
+++ hw1/hw1.cc
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    struct stat sb;
+    if (argc == 1)
+    {
+        cerr << "No path provided.\n";
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        int result = lstat(argv[i], &sb);
+        if (result != 0)
+        {
+            cout << "Error on path " << argv[i] << ".\n";
+            return 1;
+        }
+        switch (sb.st_mode & S_IFMT)
+        {
+        case S_IFIFO:
+            cout << argv[i] << " is a FIFO.\n";
+            continue;
+        case S_IFSOCK:
+            cout << argv[i] << " is a socket.\n";
+            continue;
+        }
+        auto timevals = localtime(&sb.st_mtime);
+        char buf[32];
+        strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%M:%S", timevals);
+        cout << (sb.st_mode & S_IRWXU) << " " << sb.st_size << " " << buf << " " << argv[i] << "\n";
+    }
+    return 0
+}
