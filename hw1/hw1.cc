8de784313daf8eb6da72ca3bcbf94c9fe8aabed7
aliwithay <alyammar1@hotmail.com>
2019-01-29 14:41:41 -0700

    hw1 completed

--- hw1/hw1.cc
+++ hw1/hw1.cc
@@ -29,21 +29,101 @@ int main(int argc, char *argv[])
         case S_IFSOCK:
             cout << argv[i] << " is a socket.\n";
             continue;
-	case S_IFREG:
-	    cout << "-";
-	    break;
-	case S_IFDIR:
-	    cout << "d";
-	    break;
-	case S_IFLNK:
-	    cout << "l";
-	    break;
+        case S_IFREG:
+            cout << "-";
+            break;
+        case S_IFDIR:
+            cout << "d";
+            break;
+        case S_IFLNK:
+            cout << "l";
+            break;
+        }
+        switch ((sb.st_mode & S_IRWXU) >> 6)
+        {
+        case 7:
+            cout << "rwx";
+            break;
+        case 6:
+            cout << "rw-";
+            break;
+        case 5:
+            cout << "r-x";
+            break;
+        case 4:
+            cout << "r--";
+            break;
+        case 3:
+            cout << "-wx";
+            break;
+        case 2:
+            cout << "-w-";
+            break;
+        case 1:
+            cout << "--x";
+            break;
+        case 0:
+            cout << "---";
+            break;
+        }
+        switch ((sb.st_mode & S_IRWXG) >> 3)
+        {
+        case 7:
+            cout << "rwx";
+            break;
+        case 6:
+            cout << "rw-";
+            break;
+        case 5:
+            cout << "r-x";
+            break;
+        case 4:
+            cout << "r--";
+            break;
+        case 3:
+            cout << "-wx";
+            break;
+        case 2:
+            cout << "-w-";
+            break;
+        case 1:
+            cout << "--x";
+            break;
+        default:
+            cout << "---";
+            break;
+        }
+        switch (sb.st_mode & S_IRWXO)
+        {
+        case 7:
+            cout << "rwx";
+            break;
+        case 6:
+            cout << "rw-";
+            break;
+        case 5:
+            cout << "r-x";
+            break;
+        case 4:
+            cout << "r--";
+            break;
+        case 3:
+            cout << "-wx";
+            break;
+        case 2:
+            cout << "-w-";
+            break;
+        case 1:
+            cout << "--x";
+            break;
+        default:
+            cout << "---";
+            break;
         }
         auto timevals = localtime(&sb.st_mtime);
         char buf[32];
         strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%M:%S", timevals);
-	int permissions = (sb.st_mode & S_IRWXG);
-        cout << permissions << " " << sb.st_size << " " << buf << " " << argv[i] << "\n";
+        cout << " " << sb.st_size << " " << buf << " " << argv[i] << "\n";
     }
     return 0;
 }
